99001a9324eaf0521bb875ee2a73cd344e3accc7
Lucia <luciamejia03@gmail.com>
2024-03-05 09:05:20 +0100

    Scrollview

--- src/screens/HomeDetails.jsx
+++ src/screens/HomeDetails.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Linking, ScrollView } from 'react-native';
 import themeContext from "../theme/themeContext";
 
 export default function HomeDetails({ route }) {
@@ -28,52 +28,54 @@ export default function HomeDetails({ route }) {
 
   //con esto mostramos los datos de la api en la pantalla de details
   return (
-    <View style={styles.column}>
-      <Image source={{ uri: item.flags.png }} style={styles.image} resizeMode="contain" />
-      <View style={[styles.column]}>
-        <Text style={[styles.textCommon, {color: theme.color}]}>{item.name.common}</Text>
+    <ScrollView>
+      <View style={styles.column}>
+        <Image source={{ uri: item.flags.png }} style={styles.image} resizeMode="contain" />
+        <View style={[styles.column]}>
+          <Text style={[styles.textCommon, {color: theme.color}]}>{item.name.common}</Text>
 
-        <Text style={[styles.textOfficial, {color: theme.color}]}>{item.name.official}</Text>
+          <Text style={[styles.textOfficial, {color: theme.color}]}>{item.name.official}</Text>
 
-        <Text style={[styles.text, {color: theme.color}]}>
-          <Text style={styles.boldText}>• Capital: </Text> 
-          {item.capital}
-        </Text>
+          <Text style={[styles.text, {color: theme.color}]}>
+            <Text style={styles.boldText}>• Capital: </Text> 
+            {item.capital}
+          </Text>
 
-        <Text style={[styles.text, {color: theme.color}]}>
-          <Text style={styles.boldText}>• Continente: </Text> 
-          {item.continents}
-        </Text>
+          <Text style={[styles.text, {color: theme.color}]}>
+            <Text style={styles.boldText}>• Continente: </Text> 
+            {item.continents}
+          </Text>
 
-        <Text style={[styles.text, {color: theme.color}]}>
-          <Text style={styles.boldText}>• Población: </Text> 
-          {item.population} habitantes
-        </Text>
+          <Text style={[styles.text, {color: theme.color}]}>
+            <Text style={styles.boldText}>• Población: </Text> 
+            {item.population} habitantes
+          </Text>
 
-        <Text style={[styles.text, {color: theme.color}]}>
-          <Text style={styles.boldText}>• Moneda usada: </Text> 
-          {currenciesName.join('\n')}
-        </Text>
+          <Text style={[styles.text, {color: theme.color}]}>
+            <Text style={styles.boldText}>• Moneda usada: </Text> 
+            {currenciesName.join('\n')}
+          </Text>
 
-        <Text style={[styles.text, {color: theme.color}]}>
-          <Text style={styles.boldText}>• Idiomas: </Text>{languagesName.join()}
-        </Text>
+          <Text style={[styles.text, {color: theme.color}]}>
+            <Text style={styles.boldText}>• Idiomas: </Text>{languagesName.join()}
+          </Text>
 
-        <Text style={[styles.text, {color: theme.color}]}>
-          <Text style={styles.boldText}>• Área: </Text>
-          {item.area} km cuadrados
-        </Text>
+          <Text style={[styles.text, {color: theme.color}]}>
+            <Text style={styles.boldText}>• Área: </Text>
+            {item.area} km cuadrados
+          </Text>
 
-        <Text style={[styles.text, {color: theme.color}]}>
-          <Text style={styles.boldText}>• GoogleMaps: </Text>{'\n'}
-          {item.maps.googleMaps}
-        </Text>
+          <Text style={[styles.text, {color: theme.color}]}>
+            <Text style={styles.boldText}>• GoogleMaps: </Text>{'\n'}
+            {item.maps.googleMaps}
+          </Text>
 
-        <TouchableOpacity onPress={() => Linking.openURL(`${item.maps.googleMaps}`)}>
-          <Text style={[styles.text, styles.boldText, styles.ml,{ color: '#3498db'}]}>Ir a la dirección en google maps</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => Linking.openURL(`${item.maps.googleMaps}`)}>
+            <Text style={[styles.text, styles.boldText, styles.ml,{ color: '#3498db'}]}>Ir a la dirección en google maps</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
